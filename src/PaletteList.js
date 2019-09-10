9eea9b4e882737cce7e71727cf1ce41685d92da0
Mario <mjvinti@gmail.com>
2019-09-09 21:59:09 -0400

    add bg svg to PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import MiniPalette from './MiniPalette';
-import { withStyles } from '@material-ui/styles';
-import styles from './styles/PaletteListStyles';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles/PaletteListStyles";
 
 class PaletteList extends Component {
   goToPalette(id) {
@@ -14,8 +14,8 @@ class PaletteList extends Component {
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1>React Colors</h1>
-            <Link to='/palette/new'>Create Palette</Link>
+            <h1 className={classes.heading}>React Colors</h1>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
